f2830722a2f70804a9ec931e8991f9d3eef216e4
flimflam-austin <68439674+austin-fleming@users.noreply.github.com>
2022-04-11 00:49:39 -0700

    feat(server): basic error handling and ping route

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,4 +1,6 @@
 import express = require("express");
+import type { Server as HttpServer } from "node:http";
+import { terminateServer } from "./utils/error-handling/terminate-server";
 
 const API_ROOT = "/api/v1";
 const HOST = "http://localhost";
@@ -7,11 +9,18 @@ const PORT = 443;
 const app = express();
 
 app.get(`${API_ROOT}/ping`, (request, response, next) => {
-  response.status(200).json({ status: "OK" });
+  response.status(200).json({ health_status: "OK" });
 });
 
-app.listen(PORT, () => {
+// TODO: will need to change to https when in production
+const server: HttpServer = app.listen(PORT, () => {
   console.log(`Server running on ${HOST}:${PORT}`);
 });
+
+const exitHandler = terminateServer(server);
+process.on("SIGINT", exitHandler(0, "SIGINT"));
+process.on("SIGTERM", exitHandler(0, "SIGTERM"));
+process.on("uncaughtException", exitHandler(1, "Unexpected Error"));
+process.on("unhandledRejection", exitHandler(1, "Unhandled Promise"));
 
 export default app;
